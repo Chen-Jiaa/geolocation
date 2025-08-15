23af2c79bd2b2355cd753fd7b93f597116c74885
Chen-Jiaa <eongchenjia@gmail.com>
2025-08-15 11:43:14 +0800

    flag only

--- src/app/api/geolocation/route.ts
+++ src/app/api/geolocation/route.ts
@@ -1,7 +1,12 @@
 import { geolocation } from '@vercel/functions';
 import { NextRequest } from 'next/server';
+
+interface GeoData {
+  flag: string | undefined
+}
  
 export function GET(request: NextRequest) {
-  const details = geolocation(request);
+  const geoFunction = geolocation as (req: NextRequest) => GeoData
+  const details = geoFunction(request);
   return Response.json(details.flag);
 }
